cb5ddee5aa6ab000c48374dc89cb2b0144735381
charan <Charan@LAPTOP-O7BBQSPU>
2020-08-28 23:45:26 +0530

    COMMIT

---
+++ pbms-zuul-api-gateway/src/test/java/com/capg/pbms/api/PbmsZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.pbms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbmsZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
